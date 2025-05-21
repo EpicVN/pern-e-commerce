f1f3ae3a8bb2bb96099ec3c559784739b5110eee
EpicVN <ledangkhoahi3@gmail.com>
2025-05-21 16:09:14 +0700

    feat(api): setup initial backend routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createProduct, getAllProducts } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/", getAllProducts)
+productRouter.post("/", createProduct)
+
+export default productRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,20 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(helmet()); // Set security HTTP headers
+app.use(morgan("dev")); // Log the requests
 
-app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
-});
+app.use("/api/products", productRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
