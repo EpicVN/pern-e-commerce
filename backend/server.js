d524e8cde40c4d512b457862dcfece8028ed3010
EpicVN <ledangkhoahi3@gmail.com>
2025-05-21 18:07:53 +0700

    chore(db): configure Neon PostgreSQL connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRoutes.js";
+import { sql } from "./configs/db.js";
 
 dotenv.config();
 
@@ -18,6 +19,26 @@ app.use(morgan("dev")); // Log the requests
 
 app.use("/api/products", productRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
